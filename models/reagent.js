225d237751107c54f4c795457aef1dbdac43922c
fluffmickgruff <jake.daniel.francis@gmail.com>
2021-05-11 05:53:34 -0400

    returns reagent dates as human readable strings MM/dd/yyyy

--- models/reagent.js
+++ models/reagent.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const format = require('date-fns/format')
 
 const reagentSchema = new mongoose.Schema({
   name: String,
@@ -20,6 +21,8 @@ const reagentSchema = new mongoose.Schema({
 reagentSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
+    returnedObject.date = format(new Date(returnedObject.date), 'MM/dd/yyyy')
+    returnedObject.expiration = format(new Date(returnedObject.expiration), 'MM/dd/yyyy')
     delete returnedObject._id
     delete returnedObject.__v
   }
